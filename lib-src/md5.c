e88679428f6cfaed929ca9234f09a75e1a344e68
Paolo Bonzini <bonzini@gnu.org>
2006-09-07 09:45:54 +0000

    remove $Id$ tags
    git-archimport-id: bonzini@gnu.org--2004b/smalltalk--devo--2.2--patch-125

--- lib-src/md5.c
+++ lib-src/md5.c
@@ -17,7 +17,6 @@
    You should have received a copy of the GNU General Public License
    along with this program; if not, write to the Free Software Foundation,
    Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
-   $Id: md5.c,v 1.2 2001/06/09 15:20:36 ntiffin Exp $
    
    */
 
