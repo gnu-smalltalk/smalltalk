ea24f9c71c72800643543f41599eb40c316cd26c
Paolo Bonzini <bonzini@gnu.org>
2007-03-28 06:35:29 +0000

    fix parsing of #-123
    2007-03-28  Paolo Bonzini  <bonzini@gnu.org>
    
    	* libgst/lex.c: Fix parsing of #-123.
    
    git-archimport-id: bonzini@gnu.org--2004b/smalltalk--devo--2.2--patch-286
    git-archimport-id: bonzini@gnu.org--2004b/smalltalk--devo--2.2--patch-287

--- libgst/lex.c
+++ libgst/lex.c
@@ -140,7 +140,14 @@ static int char_literal (int c,
 
 /* Parse a binary operator.  C is the first symbol in the selector */
 static int scan_bin_op (int c,
-			 YYSTYPE * lvalp);
+			YYSTYPE * lvalp);
+
+/* Actual work for scan_bin_op is done here.  MAYBE_NUMBER is false if
+   we cannot parse a negative number in this context.  */ 
+static int scan_bin_op_1 (int c,
+			  YYSTYPE * lvalp,
+			  mst_Boolean maybe_number);
+
 
 /* Parse a string literal.  C is '\'' */
 static int string_literal (int c,
@@ -485,7 +492,7 @@ scan_symbol (int c,
   /* We can read a binary operator and return a SYMBOL_LITERAL,... */
   if (CHAR_TAB (ic)->char_class & BIN_OP_CHAR)
     {
-      scan_bin_op (ic, lvalp);
+      scan_bin_op_1 (ic, lvalp, false);
       return SYMBOL_LITERAL;
     }
 
@@ -517,8 +524,9 @@ scan_symbol (int c,
 
 
 int
-scan_bin_op (int c,
-	      YYSTYPE *lvalp)
+scan_bin_op_1 (int c,
+	       YYSTYPE *lvalp,
+	       mst_Boolean maybe_number)
 {
   char buf[3];
   int ic;
@@ -549,7 +557,7 @@ scan_bin_op (int c,
 
       /* We come here also for a negative number, which we handle
          specially.  */
-      if (c == '-' && is_digit (ic))
+      if (maybe_number && c == '-' && is_digit (ic))
 	return (scan_number ('-', lvalp));
 
       buf[1] = 0;
@@ -563,6 +571,13 @@ scan_bin_op (int c,
 
   else
     return (BINOP);
+}
+
+int
+scan_bin_op (int c,
+	     YYSTYPE *lvalp)
+{
+  return scan_bin_op_1 (c, lvalp, true);
 }
 
 int
