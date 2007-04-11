690696f342b9377e4809ec1cfebcf8eb560b2b94
Paolo Bonzini <bonzini@gnu.org>
2007-04-11 13:15:03 +0000

    adjust testcases for new syntax
    git-archimport-id: bonzini@gnu.org--2004b/smalltalk--devo--2.2--patch-298

--- libgst/comp.h
+++ libgst/comp.h
@@ -266,7 +266,8 @@ extern mst_Boolean _gst_untrusted_methods
 
 /* Given a tree_node of type TREE_KEYWORD_LIST, pick out and concatenate
    the keywords, turn them into a symbol OOP and return that symbol.  */
-extern OOP _gst_compute_keyword_selector (tree_node selectorExpr);
+extern OOP _gst_compute_keyword_selector (tree_node selectorExpr)
+  ATTRIBUTE_HIDDEN;
 
 /* Called to compile and execute an "immediate expression"; i.e. a Smalltalk
    statement that is not part of a method definition and where temporaries are
