02bd7843097d3b3deaf71c763b2d23dd99dec83f
Paolo Bonzini <bonzini@gnu.org>
2007-06-05 08:44:37 +0000

    fix a few VFS bugs
    2007-06-05  Paolo Bonzini  <bonzini@gnu.org>
    
    	* kernel/FileDescr.st: Add #shutdown.
    	* kernel/Stream.st: Wrap #linesDo: and #do: in an exception handler.
    	* kernel/VFS.st: Use #linesDo:.
    
    	* libgst/interp.h: Add PRIM_SHUTDOWN_WRITE.
    	* libgst/interp.c: Implement it.  Fix bug in PRIM_MK_TEMP.
    	* libgst/sysdep.c: Use pipes/sockets instead of pty's.
    
    	* lib-src/poll.c: Yet another MacOS workaround.
    
    git-archimport-id: bonzini@gnu.org--2004b/smalltalk--devo--2.2--patch-377

--- libgst/sysdep.c
+++ libgst/sysdep.c
@@ -927,6 +927,9 @@ _gst_file_is_executable (const char *fileName)
 }
 
 
+/* Code to use PTY's did not work on Mac OS.  I'm keeping the Unix code,
+   but it should not be used.  */
+#ifdef __MSVCRT__
 typedef struct pty_info {
   int access;
   int master;
@@ -1028,7 +1031,7 @@ open_pty (pty_info *pty)
 		      return pty->master;
 		    }
 
-		  pty->slavenam[strlen ("/dev/")] = 'p';
+		  slavenam[strlen ("/dev/")] = 'p';
 		  close (pty->master);
 		}		/* open master */
 	    }			/* hex digit */
@@ -1291,6 +1294,118 @@ _gst_open_pipe (const char *command,
   else
     return (master);
 }
+#else
+
+/* Use sockets or pipes.  */
+int
+_gst_open_pipe (const char *command,
+		const char *mode)
+{
+  int fd[2];
+  int our_fd, child_fd;
+  int master;
+  int access;
+  int result;
+
+  access = strchr (mode, '+') ? O_RDWR :
+    (mode[0] == 'r' ? O_RDONLY : O_WRONLY);
+
+  if (access == O_RDWR)
+    {
+      result = socketpair (AF_UNIX, SOCK_STREAM, 0, fd);
+      our_fd = fd[1];
+      child_fd = fd[0];
+    }
+  else
+    {
+      result = pipe (fd);
+      our_fd = access == O_RDONLY ? fd[0] : fd[1];
+      child_fd = access == O_RDONLY ? fd[1] : fd[0];
+    }
+
+  if (result == -1)
+    return -1;
+
+  _gst_set_signal_handler (SIGPIPE, SIG_DFL);
+
+#ifdef HAVE_SPAWNL
+  {
+    /* Prepare file descriptors, saving the old ones so that we can keep
+       them.  */
+    int save_stdin = -1, save_stdout = -1, save_stderr = -1;
+    if (access != O_WRONLY)
+      {
+        save_stdout = dup (1);
+        save_stderr = dup (2);
+        dup2 (child_fd, 1);
+        dup2 (child_fd, 2);
+      }
+    if (access != O_RDONLY)
+      {
+        save_stdin = dup (0);
+        dup2 (child_fd, 0);
+      }
+
+#ifdef __CYGWIN__
+    result = spawnl (P_NOWAIT, "/bin/sh", "/bin/sh", "-c", command, NULL);
+#else
+    result = spawnl (P_NOWAIT, getenv("COMSPEC"), "/C", command, NULL);
+#endif
+
+    if (save_stdin != -1)
+      {
+        dup2 (save_stdin, 0);
+        close (save_stdin);
+      }
+
+    if (save_stdout != -1)
+      {
+        dup2 (save_stdout, 1);
+        close (save_stdout);
+      }
+
+    if (save_stderr != -1)
+      {
+        dup2 (save_stderr, 2);
+        close (save_stderr);
+      }
+  }
+#else /* !HAVE_SPAWNL */
+  /* We suppose it is a system that has fork.  */
+  result = fork ();
+  if (result == 0)
+    {
+      /* Child process */
+      close (our_fd);
+      if (access != O_WRONLY)
+        {
+          dup2 (child_fd, 1);
+          dup2 (child_fd, 2);
+        }
+      if (access != O_RDONLY)
+        dup2 (child_fd, 0);
+
+      _exit (system (command) >= 0);
+      /*NOTREACHED*/
+    }
+
+#endif /* !HAVE_SPAWNL */
+
+  close (child_fd);
+  _gst_set_signal_handler (SIGPIPE, SIG_IGN);
+
+  if (result == -1)
+    {
+      int save_errno;
+      save_errno = errno;
+      close (our_fd);
+      errno = save_errno;
+      return (-1);
+    }
+  else
+    return (our_fd);
+}
+#endif
 
 int
 _gst_open_file (const char *filename,
