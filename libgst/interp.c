7f945ff9194aabe2ed475fb7474105b12c7ed82b
Paolo Bonzini <bonzini@gnu.org>
2007-08-13 12:31:31 +0000

    fix GC bug in _gst_prepare_execution_environment
    Patches applied:
    
     * bonzini@gnu.org--2004b/smalltalk--devo--2.2--patch-514
       fix GC bug in _gst_prepare_execution_environment
    
    
    git-archimport-id: bonzini@gnu.org--2004b/smalltalk--stable--2.3--patch-99

--- libgst/interp.c
+++ libgst/interp.c
@@ -2073,7 +2073,8 @@ OOP
 _gst_prepare_execution_environment (void)
 {
   gst_method_context dummyContext;
-  OOP dummyContextOOP;
+  OOP dummyContextOOP, processOOP;
+  inc_ptr inc = INC_SAVE_POINTER ();
 
   empty_context_stack ();
   dummyContext = alloc_stack_context (4);
@@ -2096,7 +2097,12 @@ _gst_prepare_execution_environment (void)
   dummyContextOOP = alloc_oop (dummyContext,
 			       _gst_mem.active_flag | F_POOLED | F_CONTEXT);
 
-  return (create_callin_process (dummyContextOOP));
+
+  INC_ADD_OOP (dummyContextOOP);
+  processOOP = create_callin_process (dummyContextOOP);
+
+  INC_RESTORE_POINTER (inc);
+  return (processOOP);
 }
 
 OOP
