0e59cd2dc47fdd253cee874373f5a976dcb59994
Paolo Bonzini <bonzini@gnu.org>
2007-10-03 11:47:29 +0000

    merge from scompall
    Patches applied:
    
     * scompall@nocandysw.com--2007-nocandy/smalltalk--backstage--2.2--patch-62
       support (?i:...) in regexps
    
     * scompall@nocandysw.com--2007-nocandy/smalltalk--backstage--2.2--patch-63
       use ASCII for regexes instead of Latin-1
    
     * scompall@nocandysw.com--2007-nocandy/smalltalk--backstage--2.2--patch-64
       support new syntax with STInST compiler
    
    git-archimport-id: bonzini@gnu.org--2004b/smalltalk--devo--2.2--patch-580

--- libgst/re.c
+++ libgst/re.c
@@ -88,6 +88,7 @@ static const char *compileRegex (OOP patternOOP,
 				 struct pre_pattern_buffer *regex);
 static struct pre_pattern_buffer *allocateNewRegex (void);
 static void markRegexAsMRU (int i);
+static void init_re (void);
 
 static RegexCacheEntry cache[REGEX_CACHE_SIZE];
 
@@ -207,10 +208,7 @@ _gst_re_make_cacheable (OOP patternOOP)
   int i;
 
   if (!regexClassOOP)
-    {
-      regexClassOOP = _gst_class_name_to_oop ("Regex");
-      resultsClassOOP = _gst_class_name_to_oop ("Kernel.MatchingRegexResults");
-    }
+    init_re ();
 
   if (!IS_OOP_READONLY (patternOOP))
     return patternOOP;
@@ -280,10 +278,7 @@ make_re_results (OOP srcOOP, struct pre_registers *regs)
     return _gst_nil_oop;
 
   if (!regexClassOOP)
-    {
-      regexClassOOP = _gst_class_name_to_oop ("Regex");
-      resultsClassOOP = _gst_class_name_to_oop ("Kernel.MatchingRegexResults");
-    }
+    init_re ();
 
   resultsOOP = _gst_object_alloc (resultsClassOOP, 0);
   results = (gst_registers) OOP_TO_OBJ (resultsOOP);
@@ -375,3 +370,33 @@ _gst_re_match (OOP srcOOP, OOP patternOOP, int from, int to)
   return res;
 }
 
+
+
+/* Initialize regex.c */
+static void
+init_re (void)
+{
+  /* This is a ASCII downcase-table.  We don't make any assumptions
+     about what bytes >=128 are, so can't downcase them. */
+  static const char casetable[256] =
+    {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 
+     16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 
+     ' ', '!', '"', '#', '$', '%', '&', '\'', '(', ')', '*', '+', ',', '-', '.', '/', 
+     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ':', ';', '<', '=', '>', '?', 
+     '@', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '[', '\\', ']', '^', '_', 
+     '`', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 
+     'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '{', '|', '}', '~', 127, 
+     128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139, 140, 141, 142, 143, 
+     144, 145, 146, 147, 148, 149, 150, 151, 152, 153, 154, 155, 156, 157, 158, 159, 
+     160, 161, 162, 163, 164, 165, 166, 167, 168, 169, 170, 171, 172, 173, 174, 175, 
+     176, 177, 178, 179, 180, 181, 182, 183, 184, 185, 186, 187, 188, 189, 190, 191, 
+     192, 193, 194, 195, 196, 197, 198, 199, 200, 201, 202, 203, 204, 205, 206, 207, 
+     208, 209, 210, 211, 212, 213, 214, 215, 216, 217, 218, 219, 220, 221, 222, 223, 
+     224, 225, 226, 227, 228, 229, 230, 231, 232, 233, 234, 235, 236, 237, 238, 239, 
+     240, 241, 242, 243, 244, 245, 246, 247, 248, 249, 250, 251, 252, 253, 254, 255};
+  pre_set_casetable (casetable);
+
+  regexClassOOP = _gst_class_name_to_oop ("Regex");
+  resultsClassOOP = _gst_class_name_to_oop ("Kernel.MatchingRegexResults");
+}
