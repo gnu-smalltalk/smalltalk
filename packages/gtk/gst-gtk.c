72d7543e830fe9958f0ff4f4e9b9df9a0e6aacf6
Paolo Bonzini <bonzini@gnu.org>
2008-09-20 19:14:53 +0200

    include a backtrace in Gtk+/GLib log messages
    packages/gtk:
    2008-09-20  Paolo Bonzini  <bonzini@gnu.org>
    
    	* gst-gtk.c: Include a backtrace in log messages.

--- packages/gtk/gst-gtk.c
+++ packages/gtk/gst-gtk.c
@@ -845,8 +845,34 @@ adjustment_get_page_size (GtkAdjustment *adj)
   return (GTK_ADJUSTMENT(adj)->page_size);
 }
 
-/* Initialization.  */
+void my_log_handler (const gchar * log_domain,
+		     GLogLevelFlags log_level,
+		     const gchar * message, gpointer unused_data)
+{
+  /* Do not pass fatal flags so that we can show the backtrace.  */
+  g_log_default_handler (log_domain, log_level & G_LOG_LEVEL_MASK, message,
+			 unused_data);
 
+  switch ((log_level & G_LOG_FATAL_MASK) ? G_LOG_LEVEL_ERROR : log_level)
+    {
+    case G_LOG_LEVEL_ERROR:
+    case G_LOG_LEVEL_CRITICAL:
+    case G_LOG_LEVEL_WARNING:
+    case G_LOG_LEVEL_MESSAGE:
+      _gst_vm_proxy->showBacktrace (stderr);
+       break;
+
+    default:
+      _gst_vm_proxy->showBacktrace (stdout);
+       break;
+    }
+
+  if (log_level & G_LOG_FATAL_MASK)
+    abort ();
+}
+
+/* Initialization.  */
+ 
 void
 gst_gtk_init ()
 {
@@ -868,6 +894,22 @@ gst_initModule (proxy)
 {
   q_gst_object = g_quark_from_string ("gst_object");
   g_type_init ();
+
+  g_log_set_handler (NULL,
+		     G_LOG_LEVEL_WARNING | G_LOG_LEVEL_CRITICAL
+		     | G_LOG_LEVEL_ERROR
+		     | G_LOG_FLAG_FATAL | G_LOG_FLAG_RECURSION,
+		     my_log_handler, NULL);
+  g_log_set_handler ("Gtk",
+		     G_LOG_LEVEL_WARNING | G_LOG_LEVEL_CRITICAL
+		     | G_LOG_LEVEL_ERROR
+		     | G_LOG_FLAG_FATAL | G_LOG_FLAG_RECURSION,
+		     my_log_handler, NULL);
+  g_log_set_handler ("GLib",
+		     G_LOG_LEVEL_WARNING | G_LOG_LEVEL_CRITICAL
+		     | G_LOG_LEVEL_ERROR
+		     | G_LOG_FLAG_FATAL | G_LOG_FLAG_RECURSION,
+		     my_log_handler, NULL);
 
   _gst_vm_proxy = proxy;
   _gst_vm_proxy->defineCFunc ("gstGtkInit", gst_gtk_init);
