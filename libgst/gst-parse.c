288a66c0b082d7c534281f83a97cc1f6517fd008
Paolo Bonzini <bonzini@gnu.org>
2012-03-29 13:26:20 +0200

    fix parsing problem with lookahead at the end of a doit
    2012-03-29  Paolo Bonzini  <bonzini@gnu.org>
    
    	* tests/compiler.st: Add testcase.
    	* tests/compiler.ok: Regenerate.
    
    libgst:
    2012-03-29  Paolo Bonzini  <bonzini@gnu.org>
    
    	* libgst/gst-parse.c: Do not call _gst_free_tree in the presence
    	of lookahead, or add assertions that we can do so.  Reported by
    	Gwenael Casaccio.

--- libgst/gst-parse.c
+++ libgst/gst-parse.c
@@ -469,7 +469,12 @@ parse_doit (gst_parser *p, mst_Boolean fail_at_eof)
   else if (statement)
     {
       _gst_execute_statements (NULL, statement, UNDECLARED_TEMPORARIES, false);
-      _gst_free_tree ();
+
+      /* Because a '.' could be inserted automagically, the next token
+         value might be already on the obstack.  Do not free in that
+         case!  */
+      if (p->la_size == 0)
+        _gst_free_tree ();
     }
 
   _gst_had_error = false;
@@ -579,6 +584,7 @@ parse_eval_definition (gst_parser *p)
     {
       tmps = parse_temporaries (p, false);
       stmts = parse_statements (p, NULL, true);
+      lex_must_be (p, ']');
     }
 
   if (stmts && !_gst_had_error)
@@ -604,6 +610,7 @@ parse_eval_definition (gst_parser *p)
       _gst_had_error = false;
     }
 
+  assert (p->la_size <= 1);
   _gst_free_tree ();
   _gst_pop_temporaries_dictionary (oldDictionary);
   memcpy (p->recover, old_recover, sizeof (p->recover));
@@ -1162,6 +1169,7 @@ parse_method (gst_parser *p, int at_end)
       _gst_set_undeclared (oldUndeclared);
     }
 
+  assert (p->la_size <= 1);
   _gst_free_tree ();
   _gst_had_error = false;
   if (at_end != EOF)
